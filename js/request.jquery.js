bc9bd25cadfa057226b31f8adb42afbff8a1463e
CômeB <come.burguburu@gmail.com>
2015-01-10 22:41:07 +0100

    1.4

--- js/request.jquery.js
+++ js/request.jquery.js
@@ -1,77 +1,155 @@
-"use strict";
-var DOWN = 40;
-var UP = 38;
-var ENTER = 13;
-var index = 0;
-var oldValue = "";
-var all = 0;
+/*global $, jQuery, console */
 
-$(document).keydown(function (event) {
-    
-    if (event.which === UP) {
-        index -= 1;
-    }
-    if (event.which === DOWN) {
-        index += 1;
-    }
-    
-    if (index < 0) {
-        index = 0;
-    }
-    
-    if (event.which === ENTER) {
-        if ($(".select:first").html() !== "") {
-            $("#search_field").val($(".select:first").html());
-        } else {
-			if ($(".hover:first").html() !== "") {
-				$("#search_field").val($(".hover:first").html());
-			}
+(function ($) {
+	"use strict";
+
+	var DOWN = 40,
+		UP = 38,
+		ENTER = 13;
+		
+
+	$.fn.autocomplete = function (options) {
+	
+		if ($(this)[0].tagName.toLowerCase() !== "input" || $(this)[0].type !== "text") {
+			console.error("autocomplete : target need to be an input text");
+			return;
 		}
-        
-        $("#result").html("");
-        oldValue = $("#search_field").val();
-    }
-    
-    $("#result").off("mouseover");
-	$(".hover").removeClass("hover");
+ 
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+			link: "php/search.php",
+			autohide: false,
+			callback: null,
+			className: "select-autocomplete"
+		}, options),
+			search_field = $(this),
+			index = 0,
+			oldValue = "",
+			all = 0,
+			result = document.createElement("div"),
+			clear = $(document.createElement("span")).text("x").css({cursor: "pointer", position: "relative", top: 0, right: "15px"});
 
-    $("#result div").removeClass("select");
-    $("#result div").eq(index % all).addClass("select");
-    
-});
+		$(result).css({border: "1px solid black", width: "90%" });
+		search_field.css("width", "90%");
+		
+		if (search_field.next().length === 0 || search_field.next()[0].tagName.toLowerCase() !== "span" || search_field.next().eq(0).html() !== "x") {
+			search_field.after(result).after(clear);
+		}
 
-$("#search_field").keyup(
-    function () {
-        
-        if ($("#search_field").val() === oldValue) {
-            return;
-        }
-        
-        oldValue = $("#search_field").val();
-                   
-        $.post(
-            'php/search.php',
-            {search: $("#search_field").val()},
-            function (result) {
-				$("#result").html(result);
-				all = $("#result div").length;
-				$(".hover").removeClass("hover");
-
-				$("#result div").eq(index).addClass("select");
-				$("#result div").on("click", null, function () {$("#search_field").val($(this).html()); $("#result").html(""); });
-				$("#result div").on("mouseout", null, function () {$(this).removeClass("select hover"); });
-				$("#result div").on("mouseover", function () {
-					$(".select").removeClass("select");
-					$(".hover").removeClass("hover");
-					$(this).addClass("hover");
-					index = $(this).index();
-				});
-                                        
-            }
-        ).fail(function () {window.alert('erreur'); });
-    }
-);
-$("#clear").click(function () {
-	$("#search_field").val(null);
-	$("#result").html("");
-});
+		$(this).keydown(function (event) {
+			
+			if (event.which === UP) {
+				index -= 1;
+			}
+			if (event.which === DOWN) {
+				index += 1;
+			}
+			
+			if (index < 0) {
+				index = 0;
+			}
+			
+			if (event.which === ENTER) {
+				
+				if ($("." + settings.className).first().html() !== "") {
+					search_field.val($("." + settings.className).first().html());
+					if (settings.callback !== null) {
+						settings.callback($("." + settings.className).first().html());
+					}
+				} else {
+				
+					if ($("." + settings.className).first().html() !== "") {
+						search_field.val($("." + settings.className).first().html());
+						
+						if (settings.callback !== null) {
+							settings.callback($("." + settings.className).first().html());
+						}
+						
+					}
+				}
+				
+				$(result).html("");
+				
+				if (settings.autohide) {
+					$(result).hide();
+				}
+				
+				oldValue = search_field.val();
+			}
+			
+			$(result).off("mouseover");
+			$("." + settings.className).removeClass(settings.className);
+			
+			$(result).find("div").removeClass(settings.className);
+			$(result).find("div").eq(index % all).addClass(settings.className);
+		});
+		
+		$(this).keyup(
+			function () {
+				
+				if (search_field.val() === oldValue) {
+					return;
+				}
+				
+				oldValue = search_field.val();
+				
+				$.post(
+					settings.link,
+					{search: search_field.val()},
+					function (response) {
+						$(result).html(response).show();
+						all = $(result).find("div").length;
+						$("." + settings.className).removeClass(settings.className);
+						
+						$(result).find("div").eq(index).addClass(settings.className);
+						$(result).find("div").on("click", null,
+												 function () {
+								search_field.val($(this).html());
+																
+								if (settings.callback !== null) {
+									settings.callback($(this).html());
+								}
+								if (settings.autohide) {$(result).hide(); }
+								$(result).html("");
+							});
+						
+						$(result).find("div").on("mouseout", null,
+												 function () {
+								$(this).removeClass(settings.className);
+							});
+						
+						$(result).find("div").on("mouseover",
+											function () {
+								$("." + settings.className).removeClass(settings.className);
+								$(this).addClass(settings.className);
+								index = $(this).index();
+							});
+					}
+				);
+			}
+		);
+		
+		
+		
+		$(clear).click(function () {
+			search_field.val(null);
+			$(result).html("");
+			if (settings.autohide) {
+				$(result).hide();
+			}
+		});
+		
+		$(clear).hover(function (event) {
+			$(this).css("color", event.type === "mouseenter" ? "blue" : "black");
+		});
+	     
+		return this;
+ 
+    };
+	$.fn.autocomplete.destroy = function () {
+		$($(this).result).remove();
+		$($(this).clear).remove();
+		
+	};
+ 
+}(jQuery));
